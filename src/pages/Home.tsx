e67f96f88b339c4cb755b4e30c7bbeb18e0d1fe1
caceresrene <rene@outlook.my>
2022-09-13 16:54:52 -0300

    style: add classnames to searchbox

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,18 @@ function Home() {
 	return (
 		<>
 			<Header title='Reguleque' />
-			<main className='mx-auto max-w-6xl p-4 text-font'>
-				<SearchBox />
+			<main className='mx-auto max-w-6xl px-4 text-font font'>
+				<SearchBox
+					placeholder='Buscar funcionarios'
+					classNames={{
+						form: 'my-4 flex border-gray-300 border rounded-md',
+						input: 'flex-auto rounded order-2 px-1 py-1.5 focus:outline-none',
+						submitIcon: 'h-6 w-6 p-1 order-1 fill-font',
+						reset: 'order-3',
+						resetIcon: 'h-5 w-5 p-1 fill-font',
+					}}
+					autoFocus={true}
+				/>
 				<Hits
 					hitComponent={Hit}
 					classNames={{ list: 'grid grid-cols-1 lg:grid-cols-3 gap-2' }}
