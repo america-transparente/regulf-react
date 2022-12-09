c135c07d3a4022975239479b7518df1309471f67
panquequelol <rene@outlook.my>
2022-12-09 17:54:48 -0300

    refactor: solve type issues and reduce hit interface fields

--- src/components/HitCard.tsx
+++ src/components/HitCard.tsx
@@ -9,8 +9,31 @@ import {
 } from "@heroicons/react/24/outline";
 import { Dialog } from "@headlessui/react";
 import { Button, Card } from "@america-transparente/ui/core";
-import Hit from "../interface/hit";
+import type Hit from "../interface/hit";
 import { formatDate } from "../utils";
+
+// interface Hit {
+//   nombre: string;
+//   remuneración_bruta_mensual: string;
+//   remuneración_líquida_mensual: string;
+//   mes: string;
+//   año: string;
+//   nombre_organismo: string;
+//   tipo_cargo: string;
+//   región: string;
+//   tipo_calificación_profesional: string;
+//   fecha_ingreso: string;
+//   fecha_egreso: string;
+//   fecha_término: string;
+//   tipo_contrato: string;
+//   unidad_monetaria: string;
+//   viáticos: string;
+//   asignaciones: string;
+//   horas_diurnas: string;
+//   horas_nocturnas: string;
+//   horas_festivas: string;
+//   observaciones: string;
+// }
 
 interface HitCardProps {
   hit: Hit;
